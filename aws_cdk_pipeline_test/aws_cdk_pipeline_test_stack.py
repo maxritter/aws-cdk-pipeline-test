31e579e0a6fc58109d74c35b0f62e4721b0a97ca
Max Ritter <max.ritter@netlight.com>
2020-10-21 15:54:06 +0200

    Fixing repo branch

--- aws_cdk_pipeline_test/aws_cdk_pipeline_test_stack.py
+++ aws_cdk_pipeline_test/aws_cdk_pipeline_test_stack.py
@@ -4,9 +4,9 @@ from aws_cdk.pipelines import CdkPipeline, SimpleSynthAction
 import aws_cdk.aws_codepipeline as codepipeline
 import aws_cdk.aws_codepipeline_actions as codepipeline_actions
 
-class AwsCdkPipelineTestStack(core.Stack):
+class AwsCdkPipelineTestStack(Stack):
 
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         source_artifact = codepipeline.Artifact()
